0612abc575c1d9cda93e59506ebdc234f9d09c4a
Tiago Marchetti Lyra <tiago.mlyra@gmail.com>
2024-04-01 12:40:13 -0300

    [UPDATE] Acompanhamento Final - Aula 26/02

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -1,12 +1,34 @@
-using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class EnemyScript : MonoBehaviour
 {
     [SerializeField] private GameObject target;
+    [SerializeField] private float minSpeed;
+    [SerializeField] private float maxSpeed;
 
+    private float speed;
+    
     private void Start()
     {
-        
+        target = GameObject.FindGameObjectWithTag("Player");
+        speed = Random.Range(minSpeed, maxSpeed);
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.transform.position, speed * Time.deltaTime);
+
+        Vector3 point = target.transform.position;
+        point.y = transform.position.y;
+        transform.LookAt(point);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Projectile"))
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
